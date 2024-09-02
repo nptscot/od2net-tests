fa0ee3aa88c06b53afb72ec8404ec36a8c678578
robinlovelace <rob00x@gmail.com>
2024-09-02 20:08:10 +0100

    Update R version
    See https://github.com/nptscot/internal/issues/57

--- R/setup.R
+++ R/setup.R
@@ -1,107 +1,60 @@
 # Aim: generate input data for od2net with R
 
-# TODO: neaten this up:
-check_and_change_directory = function(name) {
-    if (!file.exists("input")) {
-        if (dir.exists(name)) {
-            message("Changing working directory to ", name)
-            setwd(name)
-        } else {
-            stop("Please run this script from the root of the repository")
-        }
-    }
+getbbox_from_zones = function() {
+  zones = sf::st_read("input/zones.geojson")
+  bbox = sf::st_bbox(zones)
+  paste0(bbox, collapse = ",")
 }
 
 make_osm = function() {
-    # Check you're in the right working directory and if not cd
-    check_and_change_directory("examples/lisbon")
     # TODO: use osmextract to download the file?
-    # TODO: new function check_and_download_file()?
-    if (!file.exists("input/portugal-latest.osm.pbf")) {
-      download.file(
-          url = "https://download.geofabrik.de/europe/portugal-latest.osm.pbf",
-          destfile = "input/portugal-latest.osm.pbf"
+    if (!file.exists("input/input.pbf")) {
+    download.file(
+          url = "https://download.geofabrik.de/europe/great-britain/scotland-latest.osm.pbf",
+          destfile = "input/input-all.osm.pbf"
       )
     }
     # Clip to Lisbon:
-    # TODO: use bbox from input/zones.geojson 
-    system(
-        "osmium extract -b -9.291687,38.673717,-9.080887,38.831685 input/portugal-latest.osm.pbf -o input/input.osm.pbf --overwrite"
-    )
+    # TODO: use bbox from input/zones.geojson
+    bb = getbbox_from_zones()
+    msg = paste0("osmium extract -b ", bb, " input/input-all.osm.pbf -o input/input.osm.pbf --overwrite")
+    system(msg)
 }
 
-make_elevation = function() {
-    # Check you're in the right working directory and if not cd
-    check_and_change_directory("examples/lisbon")
-    # Download the file
-    if (!file.exists("input/LisboaIST_10m_4326.tif")) {
-      download.file(
-          url = "https://assets.od2net.org/input/LisboaIST_10m_4326.tif",
-          destfile = "input/LisboaIST_10m_4326.tif"
-      )
-    }
-}
+# make_elevation = function() {
+#     # Check you're in the right working directory and if not cd
+#     check_and_change_directory("examples/lisbon")
+#     # Download the file
+#     if (!file.exists("input/LisboaIST_10m_4326.tif")) {
+#       download.file(
+#           url = "https://assets.od2net.org/input/LisboaIST_10m_4326.tif",
+#           destfile = "input/LisboaIST_10m_4326.tif"
+#       )
+#     }
+# }
 
-extract_centroids = function(osmInput, geojsonOutput, where="building IS NOT NULL") {
-    # Check you're in the right working directory and if not cd
-    check_and_change_directory("examples/lisbon")
-    # Use building centroids as origins
-    command = paste(
-        "ogr2ogr",
-        "-f",
-        "GeoJSON",
-        "-dialect",
-        "sqlite",
-        "-sql",
-        paste0(
-            r"("SELECT ST_Centroid(geometry) FROM multipolygons WHERE )",
-            where,
-            r"(")"
-        ),
-        geojsonOutput,
-        osmInput
-    )
-
-    system(command)
-}
 make_origins = function() {
-    extract_centroids(
-        osmInput = "input/input.osm.pbf",
-        geojsonOutput = "input/buildings.geojson"
-    )
-}
-
-make_zones = function() {
-    # Check you're in the right working directory and if not cd
-    check_and_change_directory("examples/lisbon")
-    # Download the file
-    if (!file.exists("input/zones.geojson")) {
-      download.file(
-          url = "https://github.com/U-Shift/biclar/releases/download/0.0.1/zones.geojson",
-          destfile = "input/zones.geojson"
-        )
-    }
+    buildings = sf::read_sf("input/input.osm.pbf", query = "SELECT osm_id FROM multipolygons WHERE building IS NOT NULL")
+    centroids = sf::st_centroid(buildings)
+    sf::write_sf(centroids, "input/buildings.geojson", delete_dsn = TRUE)
 }
 
 make_od = function() {
-    # Check you're in the right working directory and if not cd
-    check_and_change_directory("examples/lisbon")
-    # Download the file
-    if (!file.exists("input/od.csv")) {
-      download.file(
-          url = "https://github.com/U-Shift/biclar/releases/download/0.0.1/od.csv",
-          destfile = "input/od.csv"
-        )
-    }
+    od = readr::read_csv("https://raw.githubusercontent.com/nptscot/npt/main/data-raw/od_subset.csv")
+    od = od |>
+      dplyr::transmute(from = geo_code1, to = geo_code2, count = bicycle)
+    readr::write_csv(od, "input/od.csv")
 }
 
 main = function() {
     dir.create("input", showWarnings = FALSE)
     make_osm()
-    make_elevation()
+    # make_elevation()
     make_origins()
-    make_zones()
+    # make_zones()
     make_od()
 }
+
+
 
 main()
